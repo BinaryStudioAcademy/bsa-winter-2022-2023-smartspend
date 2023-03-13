d00759e8ce174bb0f4595fb6a2ca23285107949a
notlikethisss <anton3447@gmail.com>
2023-03-13 19:58:59 +0200

    ss-7: + base modal

---
+++ frontend/src/bundles/common/components/modal/modal.tsx
@@ -0,0 +1,46 @@
+import './modal.scss';
+
+import { type ReactNode } from 'react';
+import React, { useCallback } from 'react';
+
+interface Properties {
+    active: boolean;
+    setActive: React.Dispatch<React.SetStateAction<boolean>>;
+    onClose: () => void;
+    header?: ReactNode;
+    body?: ReactNode;
+    children: ReactNode;
+}
+
+const Modal: React.FC<Properties> = ({
+    header,
+    body,
+    onClose,
+    children,
+    active,
+    setActive,
+}) => {
+    const handleClose = useCallback(() => {
+        setActive(false);
+        onClose();
+    }, [setActive, onClose]);
+
+    return (
+        <div className={active ? 'modal active' : 'modal'}>
+            <div className="popup">
+                <button
+                    data-test-id="book-trip-popup-close"
+                    className="popup__close"
+                    onClick={handleClose}
+                >
+                    ×
+                </button>
+                <div className="modal-header">{header}</div>
+                <div className="modal-body">{body}</div>
+                <div className="modal-footer">{children}</div>
+            </div>
+        </div>
+    );
+};
+
+export { Modal };

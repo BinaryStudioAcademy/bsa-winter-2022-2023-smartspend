d746c440abed6859309a36d9534f851fe8125da3
nikita-remeslov <nikita.remeslov@binary-studio.com>
2023-03-13 15:22:26 +0100

    production: * code style

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,27 +3,27 @@ import { type ConfigEnv, defineConfig, loadEnv } from 'vite';
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 
 const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
-  const {
-    VITE_APP_PROXY_SERVER_URL,
-    VITE_APP_API_ORIGIN_URL,
-    VITE_APP_DEVELOPMENT_PORT,
-  } = loadEnv(mode, process.cwd());
+    const {
+        VITE_APP_PROXY_SERVER_URL,
+        VITE_APP_API_ORIGIN_URL,
+        VITE_APP_DEVELOPMENT_PORT,
+    } = loadEnv(mode, process.cwd());
 
-  return defineConfig({
-    build: {
-      outDir: 'build',
-    },
-    plugins: [tsconfigPathsPlugin(), reactPlugin()],
-    server: {
-      port: Number(VITE_APP_DEVELOPMENT_PORT),
-      proxy: {
-        [VITE_APP_API_ORIGIN_URL]: {
-          target: VITE_APP_PROXY_SERVER_URL,
-          changeOrigin: true,
+    return defineConfig({
+        build: {
+            outDir: 'build',
         },
-      },
-    },
-  });
+        plugins: [tsconfigPathsPlugin(), reactPlugin()],
+        server: {
+            port: Number(VITE_APP_DEVELOPMENT_PORT),
+            proxy: {
+                [VITE_APP_API_ORIGIN_URL]: {
+                    target: VITE_APP_PROXY_SERVER_URL,
+                    changeOrigin: true,
+                },
+            },
+        },
+    });
 };
 
 export default config;

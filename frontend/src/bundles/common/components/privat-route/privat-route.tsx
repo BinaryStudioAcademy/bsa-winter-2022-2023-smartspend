93f34a059e6e730257d16dc0aeb7f5964939e923
teslmik <teslmik@gmail.com>
2023-03-13 22:16:39 +0200

    ss-13: * privat route

---
+++ frontend/src/bundles/common/components/privat-route/privat-route.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom';
+
+import { AppRoute } from '../../enums/enums';
+import { useAppSelector } from '../../hooks/hooks';
+
+const PrivatRoute: React.FC<{ component: React.FC }> = ({
+    component: Component,
+}) => {
+    const { isLoggedIn } = useAppSelector((state) => state.auth);
+
+    return isLoggedIn ? (
+        <Component />
+    ) : (
+        <Navigate to={AppRoute.DASHBOARD} replace />
+    );
+};
+
+export { PrivatRoute };

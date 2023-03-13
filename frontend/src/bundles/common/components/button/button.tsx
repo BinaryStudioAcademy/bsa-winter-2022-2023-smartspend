018a918e0c5ebf664ae8726bb3cb2936560a4e20
Anatolii <anatoliy.petrenko.777@gmail.com>
2023-03-13 20:31:45 +0200

    ss-5: + BaseButton component test

--- frontend/src/bundles/common/components/button/button.tsx
+++ frontend/src/bundles/common/components/button/button.tsx
@@ -1,10 +1,36 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
 type Properties = {
-    label: string;
-    type?: 'button' | 'submit';
+    children: React.ReactNode;
+    className?: string;
+    variant?: 'primary' | 'secondary' | 'plain' | 'round';
+    size?: 'medium' | 'small';
+    disabled?: boolean;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button: React.FC<Properties> = ({ type = 'button', label }) => (
-    <button type={type}>{label}</button>
-);
+const Button: React.FC<Properties> = ({
+    children,
+    className = '',
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    onClick,
+}) => {
+    const buttonClasses = [
+        styles.button,
+        variant === 'round' ? styles.equalRounded : styles[size],
+        styles[variant + (disabled ? 'Disabled' : '')],
+        className,
+    ].join(' ');
+
+    return (
+        <button className={buttonClasses} disabled={disabled} onClick={onClick}>
+            {children}
+        </button>
+    );
+};
 
 export { Button };

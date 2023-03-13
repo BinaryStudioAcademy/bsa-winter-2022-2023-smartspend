db183048d8b9a8ca50747efbedab2f7d1c7fbb2a
teslmik <teslmik@gmail.com>
2023-03-13 19:41:25 +0200

    ss-13: + public routes

---
+++ frontend/src/bundles/common/components/public-route/public-route.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { AppRoute } from '../../enums/enums';
+import { useAppSelector } from '../../hooks/hooks';
+
+const PublicRoute: React.FC<{ component: React.FC }> = ({
+    component: Component,
+}) => {
+    const { isLoggedIn } = useAppSelector((state) => state.auth);
+
+    return isLoggedIn ? <Navigate to={AppRoute.ROOT} replace /> : <Component />;
+};
+
+export { PublicRoute };

ed52049750e90240d00b57cc9320ad1c09f8103f
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-03-14 15:16:07 +0200

    ss-3: * re-export styleguide-route.enum

--- frontend/src/bundles/common/enums/enums.ts
+++ frontend/src/bundles/common/enums/enums.ts
@@ -1,9 +1,9 @@
 export { AppRoute } from './app-route.enum.js';
 export { DataStatus } from './data-status.enum.js';
+export { StyleGuide } from './styleguide-route.enum.js';
 export {
     ApiPath,
     AppEnvironment,
     ContentType,
     ServerErrorType,
 } from 'shared/build/index.js';
-export { StyleGuide } from './styleguide-route.enum.js';

8f9f19e92edb90204d7700e3fb612f3cd8349f6a
teslmik <teslmik@gmail.com>
2023-03-14 22:01:10 +0200

    ss-12: + add enums

---
+++ frontend/src/bundles/common/enums/doughnut-data.enum.ts
@@ -0,0 +1,6 @@
+const DoughnutData = {
+    CUTOUT: 48.5,
+    RADIUS: 58.5,
+} as const;
+
+export { DoughnutData };
--- frontend/src/bundles/common/enums/enums.ts
+++ frontend/src/bundles/common/enums/enums.ts
@@ -1,5 +1,6 @@
 export { AppRoute } from './app-route.enum.js';
 export { DataStatus } from './data-status.enum.js';
+export { DoughnutData } from './doughnut-data.enum.js';
 export {
     ApiPath,
     AppEnvironment,

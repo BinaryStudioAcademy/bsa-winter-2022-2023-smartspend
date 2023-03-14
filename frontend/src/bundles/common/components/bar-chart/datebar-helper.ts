853ff92d7021aa5979dd0b7bfa0ee517d628db7b
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-03-14 20:33:59 +0200

    ss-11: + helper for correct dispaly date

---
+++ frontend/src/bundles/common/components/bar-chart/datebar-helper.ts
@@ -0,0 +1,16 @@
+import { type DataObject } from './types-bar';
+
+const dateHelper = (array: DataObject[]): DataObject[] => {
+    return array.map((object) => {
+        return {
+            date: new Date(object.date).toLocaleDateString('en-US', {
+                month: 'short',
+                day: '2-digit',
+                year: 'numeric',
+            }),
+            value: object.value,
+        };
+    });
+};
+
+export { dateHelper };

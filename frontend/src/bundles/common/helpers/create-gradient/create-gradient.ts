3bbd5a57fe35c53fe00040a0e7298efefa752390
teslmik <teslmik@gmail.com>
2023-03-14 22:12:23 +0200

    ss-12: + helpers

---
+++ frontend/src/bundles/common/helpers/create-gradient/create-gradient.ts
@@ -0,0 +1,17 @@
+import { type ScriptableContext } from 'chart.js';
+
+const createGradientItem = (cxt: CanvasRenderingContext2D, colorArray: string[]): CanvasGradient => {
+    const value = cxt.createLinearGradient(0, 0, 100, 0);
+
+    value.addColorStop(0, colorArray[0]);
+    value.addColorStop(1, colorArray[1]);
+
+    return value;
+};
+
+const createGradients = (context: ScriptableContext<'doughnut'>, colors: string[][]): CanvasGradient[] => {
+    const context_ = context.chart.ctx;
+    return colors.map(color => createGradientItem(context_, color));
+};
+
+export { createGradients };

3ef225140c74adccbda6b489c1a189da702daeb7
unknown <demomusicevcat@gmail.com>
2023-03-15 09:24:40 +0100

    ss-27: * imports

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -2,9 +2,12 @@ export { AuthApiPath } from './bundles/auth/auth.js';
 export {
     type UserGetAllItemResponseDto,
     type UserGetAllResponseDto,
+    type UserSignInRequestDto,
+    type UserSignInResponseDto,
     type UserSignUpRequestDto,
     type UserSignUpResponseDto,
     UsersApiPath,
+    userSignInValidationSchema,
     userSignUpValidationSchema,
 } from './bundles/users/users.js';
 export {

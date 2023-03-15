d1c3bc077d0edf5ca2863453ac90e2b2e8afa4b8
teslmik <teslmik@gmail.com>
2023-03-15 19:25:41 +0200

    ss-13: * only user check

--- frontend/src/bundles/common/components/public-route/public-route.tsx
+++ frontend/src/bundles/common/components/public-route/public-route.tsx
@@ -1,27 +1,16 @@
 import { Navigate } from 'react-router-dom';
 
-import { actions as userActions } from '~/bundles/users/store';
-
 import { AppRoute } from '../../enums/enums';
-import { useAppDispatch, useAppSelector, useEffect } from '../../hooks/hooks';
+import { useAppSelector } from '../../hooks/hooks';
 
 const PublicRoute: React.FC<{ children: React.ReactElement }> = ({
     children,
 }) => {
-    const dispatch = useAppDispatch();
-    const { users } = useAppSelector(({ users }) => ({
-        users: users.users,
-    }));
     const { user } = useAppSelector(({ auth }) => ({
         user: auth.user,
     }));
-    const isAuth = users.some((user_) => user_.id === user?.id);
 
-    useEffect(() => {
-        void dispatch(userActions.loadAll());
-    }, [dispatch, user]);
-
-    if (isAuth) {
+    if (user) {
         return <Navigate to={AppRoute.DASHBOARD} />;
     }
 

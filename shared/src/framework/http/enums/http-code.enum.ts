5252f6bb2ac05d938ca997704a310a9041de548e
unknown <demomusicevcat@gmail.com>
2023-03-15 18:53:22 +0100

    ss-27: + unauthorized status code

--- shared/src/framework/http/enums/http-code.enum.ts
+++ shared/src/framework/http/enums/http-code.enum.ts
@@ -3,7 +3,8 @@ const HttpCode = {
     CREATED: 201,
     UNPROCESSED_ENTITY: 422,
     INTERNAL_SERVER_ERROR: 500,
-    NOT_FOUND: 404
+    NOT_FOUND: 404,
+    UNAUTHORIZED: 401
 } as const;
 
 export { HttpCode };

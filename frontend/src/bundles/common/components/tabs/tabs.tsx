4a1bdafd51c87074a333cfa0af81c6dd535ca182
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-03-15 13:30:27 +0200

    ss-4: * added prop to tabs to accept data

--- frontend/src/bundles/common/components/tabs/tabs.tsx
+++ frontend/src/bundles/common/components/tabs/tabs.tsx
@@ -3,16 +3,20 @@ import React from 'react';
 import { Tab } from './tab';
 import styles from './tabs.module.css';
 
-const Tabs: React.FC = () => {
+interface TabsData {
+    title: string;
+    to: string;
+}
+
+interface TabsProperties {
+    tabsData: TabsData[];
+}
+
+const Tabs: React.FC<TabsProperties> = ({ tabsData }) => {
     return (
         <nav>
             <ul className={styles.tabs}>
-                {[
-                    { title: 'Transaction', to: '/ui/' },
-                    { title: 'Overview', to: '/ui/overview' },
-                    { title: 'Budget', to: '/ui/budget' },
-                    { title: 'Wallet Settings', to: '/ui/wallet-settings' },
-                ].map((item, index) => (
+                {tabsData.map((item, index) => (
                     <Tab key={index} title={item.title} to={item.to} />
                 ))}
             </ul>
--- frontend/src/bundles/common/pages/base.tsx
+++ frontend/src/bundles/common/pages/base.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 import { Tabs } from '../components/tabs/tabs';
 
+const tabsData = [
+    { title: 'Transaction', to: '/ui/' },
+    { title: 'Overview', to: '/ui/overview' },
+    { title: 'Budget', to: '/ui/budget' },
+    { title: 'Wallet Settings', to: '/ui/wallet-settings' },
+];
+
 const Base: React.FC = () => {
     return (
         <div>
             Base Page
-            <Tabs />
+            <Tabs tabsData={tabsData} />
         </div>
     );
 };

651c6fd185e5aaeaf8e99cf754bbae425a0fea88
TarasMavrovich <taras.mavrovich@gmail.com>
2023-03-15 20:36:52 +0200

    ss-6: * CreateInputNote/className, * Base/CreateInputNote

--- frontend/src/bundles/common/components/input/app-input.tsx
+++ frontend/src/bundles/common/components/input/app-input.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 import { Input } from '~/bundles/common/components/components';
 import { useAppForm } from '~/bundles/common/hooks/hooks';
 
+import styles from './styles.module.scss';
+
 const DEFAULT_INPUT_FILTER: { note: string } = {
     note: '',
 };
 
-const CreateInput: React.FC = () => {
+const CreateInputNote: React.FC = () => {
     const { control, errors } = useAppForm<{ note: string }>({
         defaultValues: DEFAULT_INPUT_FILTER,
     });
@@ -21,8 +23,9 @@ const CreateInput: React.FC = () => {
             name="note"
             control={control}
             errors={errors}
+            className={styles.input}
         />
     );
 };
 
-export { CreateInput };
+export { CreateInputNote };
--- frontend/src/bundles/common/pages/base.tsx
+++ frontend/src/bundles/common/pages/base.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { CreateInputNote } from '../components/input/app-input';
+
 const Base: React.FC = () => {
-    return <div>Base Page</div>;
+    return (
+        <div>
+            Base Page
+            <CreateInputNote />
+        </div>
+    );
 };
 
 export { Base };

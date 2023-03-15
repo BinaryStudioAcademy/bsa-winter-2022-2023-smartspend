55bbae5329373cae38e4287046ba14031990935f
unknown <demomusicevcat@gmail.com>
2023-03-15 22:01:43 +0100

    ss-27: * auth service

--- backend/src/bundles/auth/auth.service.ts
+++ backend/src/bundles/auth/auth.service.ts
@@ -5,12 +5,23 @@ import {
     type UserSignUpResponseDto,
 } from '~/bundles/users/types/types.js';
 import { type UserService } from '~/bundles/users/user.service.js';
+import { HttpError } from '~/common/exceptions/exceptions.js';
+import { HttpCode } from '~/common/http/enums/enums.js';
+import { type CryptService } from '~/common/services/crypt/crypt.service.js';
+import { tokenService } from '~/common/services/services.js';
+
+type User = {
+    id: number;
+    email: string;
+};
 
 class AuthService {
     private userService: UserService;
+    private cryptService: CryptService;
 
-    public constructor(userService: UserService) {
+    public constructor(userService: UserService, cryptService: CryptService) {
         this.userService = userService;
+        this.cryptService = cryptService;
     }
 
     public signUp(
@@ -22,11 +33,36 @@ class AuthService {
     public async signIn(
         userRequestDto: UserSignInRequestDto,
     ): Promise<UserSignInResponseDto | undefined> {
-        const user = await this.userService.find(userRequestDto);
-        if(!user) {
-            return undefined;
+        const user = await this.verifySignInCredentials(userRequestDto);
+        const token = tokenService.createToken(user);
+        return {
+            token,
+        };
+    }
+
+    private async verifySignInCredentials(
+        requestUser: UserSignInRequestDto,
+    ): Promise<User> {
+        const user = await this.userService.find(requestUser);
+        if (!user) {
+            throw new HttpError({
+                message: 'Invalid email',
+                status: HttpCode.UNAUTHORIZED,
+            });
         }
-        return { hash: user.toNewObject().passwordHash };
+        const userNewObject = user.toNewObject();
+        const userObject = user.toObject();
+        const isEqualPassword = this.cryptService.compareSyncPassword(
+            requestUser.password,
+            userNewObject.passwordHash,
+        );
+        if (!isEqualPassword) {
+            throw new HttpError({
+                message: 'Invalid Password',
+                status: HttpCode.UNAUTHORIZED,
+            });
+        }
+        return userObject;
     }
 }
 

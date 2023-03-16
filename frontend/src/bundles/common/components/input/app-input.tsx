a7f2fd16dfccba3963cf4dbf2fc75cedccc2ddf5
TarasMavrovich <taras.mavrovich@gmail.com>
2023-03-16 19:35:46 +0200

    ss-6: * base/CreateInputNote, styles.module/use_global_variables

--- frontend/src/bundles/common/components/input/app-input.tsx
+++ frontend/src/bundles/common/components/input/app-input.tsx
@@ -1,4 +1,3 @@
-// /* eslint-disable */
 import React from 'react';
 
 import { Input } from '~/bundles/common/components/components';
--- frontend/src/bundles/common/pages/base.tsx
+++ frontend/src/bundles/common/pages/base.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Button } from '../components/components';
 import { DoughnutChart } from '../components/doughnut-chart/doughnut-chart';
+import { CreateInputNote } from '../components/input/app-input';
 import { ButtonSize } from '../enums/button-size.enum';
 import { ButtonVariant } from '../enums/button-variant.enum';
 
@@ -190,6 +191,9 @@ const Base: React.FC = () => {
                 <DoughnutChart categories={categories} />
             </div>
             {/* end-Doughnut Chart------------------------------- */}
+            <div>
+                <CreateInputNote />
+            </div>
         </div>
     );
 };

a44cb75f8c48e555df03e040e1ec6a3460005b5d
teslmik <teslmik@gmail.com>
2023-03-16 10:37:03 +0200

    ss-43: * add to base page

--- frontend/src/bundles/common/pages/base.tsx
+++ frontend/src/bundles/common/pages/base.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import { CardTotal } from '../components/card-total/card-total';
+import { CardVariant } from '../enums/card-variant.enum';
+
 const Base: React.FC = () => {
-    return <div>Base Page</div>;
+    return (
+        <div>
+            Base Page
+            <div>
+                <p style={{ textAlign: 'center' }}>Card Total</p>
+                <div
+                    style={{
+                        display: 'flex',
+                        gap: '20px',
+                        justifyContent: 'space-around',
+                    }}
+                >
+                    <CardTotal
+                        title="Total Balance"
+                        sum={40.45}
+                        variant={CardVariant.ORANGE}
+                    />
+                    <CardTotal
+                        title="Total Period Change"
+                        sum={5040.54}
+                        variant={CardVariant.BLUE}
+                    />
+                    <CardTotal
+                        title="Total Period Expenses"
+                        sum={-9700.34}
+                        variant={CardVariant.WHITE}
+                    />
+                    <CardTotal
+                        title="Total Balance"
+                        sum={7600.34}
+                        variant={CardVariant.VIOLET}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export { Base };

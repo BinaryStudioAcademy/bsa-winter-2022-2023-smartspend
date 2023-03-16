80e967ca33df3839d529f2dc56d5c03793301135
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-03-16 10:45:53 +0200

    ss-4: * merge development

--- frontend/src/bundles/common/pages/base.tsx
+++ frontend/src/bundles/common/pages/base.tsx
@@ -1,158 +1,169 @@
 import React from 'react';
 
 import { Button } from '../components/components';
+import { Tabs } from '../components/tabs/tabs';
 import { ButtonSize } from '../enums/button-size.enum';
 import { ButtonVariant } from '../enums/button-variant.enum';
 
+const tabsData = [
+    { title: 'Transaction', to: '/ui/' },
+    { title: 'Overview', to: '/ui/overview' },
+    { title: 'Budget', to: '/ui/budget' },
+    { title: 'Wallet Settings', to: '/ui/wallet-settings' },
+];
+
 const Base: React.FC = () => {
     return (
-        <div
-            style={{
-                height: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
+        <>
+            <Tabs tabsData={tabsData} />
             <div
                 style={{
+                    height: '100vh',
                     display: 'flex',
                     flexDirection: 'column',
-                    alignItems: 'start',
-                    gap: '20px',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                 }}
             >
                 <div
                     style={{
                         display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'start',
                         gap: '20px',
                     }}
                 >
-                    <Button
-                        variant={ButtonVariant.PRIMARY}
-                        size={ButtonSize.MEDIUM}
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '20px',
+                        }}
                     >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                    <Button
-                        variant={ButtonVariant.PRIMARY}
-                        size={ButtonSize.MEDIUM}
-                        disabled={true}
+                        <Button
+                            variant={ButtonVariant.PRIMARY}
+                            size={ButtonSize.MEDIUM}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                        <Button
+                            variant={ButtonVariant.PRIMARY}
+                            size={ButtonSize.MEDIUM}
+                            disabled={true}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                    </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '20px',
+                        }}
                     >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        gap: '20px',
-                    }}
-                >
-                    <Button
-                        variant={ButtonVariant.SECONDARY}
-                        size={ButtonSize.MEDIUM}
+                        <Button
+                            variant={ButtonVariant.SECONDARY}
+                            size={ButtonSize.MEDIUM}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                        <Button
+                            variant={ButtonVariant.SECONDARY}
+                            size={ButtonSize.MEDIUM}
+                            disabled={true}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                    </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '20px',
+                        }}
                     >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                    <Button
-                        variant={ButtonVariant.SECONDARY}
-                        size={ButtonSize.MEDIUM}
-                        disabled={true}
+                        <Button
+                            variant={ButtonVariant.PRIMARY}
+                            size={ButtonSize.SMALL}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                        <Button
+                            variant={ButtonVariant.PRIMARY}
+                            size={ButtonSize.SMALL}
+                            disabled={true}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                    </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '20px',
+                        }}
                     >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        gap: '20px',
-                    }}
-                >
-                    <Button
-                        variant={ButtonVariant.PRIMARY}
-                        size={ButtonSize.SMALL}
+                        <Button
+                            variant={ButtonVariant.SECONDARY}
+                            size={ButtonSize.SMALL}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                        <Button
+                            variant={ButtonVariant.SECONDARY}
+                            size={ButtonSize.SMALL}
+                            disabled={true}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                    </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '20px',
+                        }}
                     >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                    <Button
-                        variant={ButtonVariant.PRIMARY}
-                        size={ButtonSize.SMALL}
-                        disabled={true}
+                        <Button
+                            variant={ButtonVariant.PLAIN}
+                            size={ButtonSize.SMALL}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                        <Button
+                            variant={ButtonVariant.PLAIN}
+                            size={ButtonSize.SMALL}
+                            disabled={true}
+                        >
+                            <span>+</span>
+                            <span>Button</span>
+                            <span>˅</span>
+                        </Button>
+                    </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '20px',
+                        }}
                     >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        gap: '20px',
-                    }}
-                >
-                    <Button
-                        variant={ButtonVariant.SECONDARY}
-                        size={ButtonSize.SMALL}
-                    >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                    <Button
-                        variant={ButtonVariant.SECONDARY}
-                        size={ButtonSize.SMALL}
-                        disabled={true}
-                    >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        gap: '20px',
-                    }}
-                >
-                    <Button
-                        variant={ButtonVariant.PLAIN}
-                        size={ButtonSize.SMALL}
-                    >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                    <Button
-                        variant={ButtonVariant.PLAIN}
-                        size={ButtonSize.SMALL}
-                        disabled={true}
-                    >
-                        <span>+</span>
-                        <span>Button</span>
-                        <span>˅</span>
-                    </Button>
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        gap: '20px',
-                    }}
-                >
-                    <Button variant={ButtonVariant.ROUND}>+</Button>
+                        <Button variant={ButtonVariant.ROUND}>+</Button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 

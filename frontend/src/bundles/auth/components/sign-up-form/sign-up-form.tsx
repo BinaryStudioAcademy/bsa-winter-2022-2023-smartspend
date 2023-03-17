7ae724a1162663de55256f43d60e8f0c26a9e06e
teslmik <teslmik@gmail.com>
2023-03-17 15:18:14 +0200

    ss-56: * style & bale markup

--- frontend/src/bundles/auth/components/sign-up-form/sign-up-form.tsx
+++ frontend/src/bundles/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,6 +1,4 @@
-import fbIcon from '~/assets/img/facebook-icon.svg';
-import googleIcon from '~/assets/img/google-icon.svg';
-import { Button, Input, Link } from '~/bundles/common/components/components';
+import { Button, Input } from '~/bundles/common/components/components';
 import { ButtonType } from '~/bundles/common/enums/enums';
 import { useAppForm, useCallback } from '~/bundles/common/hooks/hooks';
 import {
@@ -8,7 +6,6 @@ import {
     userSignUpValidationSchema,
 } from '~/bundles/users/users';
 
-import { AuthApiPath } from '../../enums/enums';
 import { DEFAULT_SIGN_UP_PAYLOAD } from './constants/constants';
 import styles from './styles.module.scss';
 
@@ -30,44 +27,39 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }) => {
     );
 
     return (
-        <div className={styles.signup__wrapper}>
-            <div className="signup__content">
-                <div className="signup__header">
-                    <h2>Sign Up</h2>
-                    <Link to={AuthApiPath.SIGN_IN}>Have an account? Log in</Link>
-                </div>
-                <form onSubmit={handleFormSubmit}>
-                    <p>
-                        <Input
-                            type="text"
-                            label="Email"
-                            placeholder="Enter your email"
-                            name="email"
-                            control={control}
-                            errors={errors}
-                        />
-                    </p>
-                    <p>
-                        <Input
-                            type="text"
-                            label="Password"
-                            placeholder="Enter your password"
-                            name="password"
-                            control={control}
-                            errors={errors}
-                        />
-                    </p>
-                    <Button type={ButtonType.SUBMIT}>Sign up</Button>
-                </form>
-                <div className="singup__footer">
-                    <p>Or Sign Up with</p>
-                    <div className="singup__social">
-                        <img src={googleIcon} alt="googleIcon" />
-                        <img src={fbIcon} alt="facebookIcon" />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <form className={styles.form} onSubmit={handleFormSubmit}>
+            <p>
+                <Input
+                    type="text"
+                    label="Email"
+                    placeholder="Enter your email"
+                    name="email"
+                    control={control}
+                    errors={errors}
+                />
+            </p>
+            <p>
+                <Input
+                    type="text"
+                    label="Password"
+                    placeholder="Enter your password"
+                    name="password"
+                    control={control}
+                    errors={errors}
+                />
+            </p>
+            <p>
+                <Input
+                    type="text"
+                    label="Confirm password"
+                    placeholder="Confirm your password"
+                    name="password"
+                    control={control}
+                    errors={errors}
+                />
+            </p>
+            <Button type={ButtonType.SUBMIT}>Sign up</Button>
+        </form>
     );
 };
 

208dce0ae7117bbfe1dc6197a5bb4827caab41bc
teslmik <teslmik@gmail.com>
2023-03-17 23:14:36 +0200

    ss-73: + add header component

--- frontend/src/bundles/auth/components/sign-up-form/constants/constants.ts
+++ frontend/src/bundles/auth/components/sign-up-form/constants/constants.ts
@@ -3,7 +3,6 @@ import { type UserSignUpRequestDto } from '~/bundles/users/users';
 const DEFAULT_SIGN_UP_PAYLOAD: UserSignUpRequestDto = {
     email: '',
     password: '',
-    confirm: '',
 };
 
 export { DEFAULT_SIGN_UP_PAYLOAD };

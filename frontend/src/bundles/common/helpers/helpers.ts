aa8b34aeea82360bfe78a93cb2b358a7f560f2d4
teslmik <teslmik@gmail.com>
2023-03-17 14:33:44 +0200

    ss-62: + helper getText

--- frontend/src/bundles/common/helpers/helpers.ts
+++ frontend/src/bundles/common/helpers/helpers.ts
@@ -1,4 +1,5 @@
 export { createGradients } from './create-gradient/create-gradient.js';
 export { getGradientColors } from './get-gradient-colors/get-gradient-colors.js';
+export { getText } from './get-text-to-path/get-text-to-path.js';
 export { sumArray } from './sum-array/sum-array.js';
 export { configureString } from 'shared/build/index.js';

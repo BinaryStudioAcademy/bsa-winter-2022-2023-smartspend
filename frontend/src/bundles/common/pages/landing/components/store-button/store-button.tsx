006fb1bc7406fc4089605da7b9f1e530c7e2d1c2
unknown <demomusicevcat@gmail.com>
2023-03-17 11:53:05 +0100

    ss-57: + store button

---
+++ frontend/src/bundles/common/pages/landing/components/store-button/store-button.tsx
@@ -0,0 +1,32 @@
+import { Button } from '~/bundles/common/components/components';
+import { ButtonVariant } from '~/bundles/common/enums/button-variant.enum';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+    title: string;
+    body: string;
+    iconPath: string;
+    storeAlt: string;
+};
+
+const StoreButton: React.FC<Properties> = ({
+    title,
+    body,
+    iconPath,
+    storeAlt,
+}) => {
+    return (
+        <Button variant={ButtonVariant.PLAIN} className={styles.store_button}>
+            <div className={styles.container}>
+                <img src={iconPath} alt={storeAlt} />
+                <div className={styles.details_container}>
+                    <h1 className={styles.title}>{title}</h1>
+                    <p className={styles.body}>{body}</p>
+                </div>
+            </div>
+        </Button>
+    );
+};
+
+export { StoreButton };

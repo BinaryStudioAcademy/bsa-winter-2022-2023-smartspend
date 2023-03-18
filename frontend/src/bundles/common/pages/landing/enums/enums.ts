f76a91cf19695d36385398bdf016b8e8da0ac80a
unknown <demomusicevcat@gmail.com>
2023-03-18 15:18:30 +0100

    ss-57: + step info button variant enum

--- frontend/src/bundles/common/pages/landing/enums/enums.ts
+++ frontend/src/bundles/common/pages/landing/enums/enums.ts
@@ -1,3 +1,4 @@
 export { DoughnutChartCartVariant } from './doughnut-chart-card-variant.enum';
+export { StepInfoButtonVariant } from './step-info-button-variant.enum';
 export { StepInfoPanelVariant } from './step-info-panel-variant.enum';
 export { ButtonVariant } from '~/bundles/common/enums/button-variant.enum';

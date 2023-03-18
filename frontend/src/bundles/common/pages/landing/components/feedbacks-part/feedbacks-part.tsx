6a81136ca5dbcf6c1c682ff9e667377cca441a2e
unknown <demomusicevcat@gmail.com>
2023-03-18 19:18:40 +0100

    ss-57: + feedbacks part scaffolding

---
+++ frontend/src/bundles/common/pages/landing/components/feedbacks-part/feedbacks-part.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+const FeedbacksPart: React.FC = () => {
+    return (
+        <div className={styles.container} >
+            
+        </div>
+    );
+};
+
+export { FeedbacksPart };

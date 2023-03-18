ffc97bd718e28300a05317d28756bea5628cf1eb
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-03-18 21:07:32 +0100

    ss-57: + app route import

--- frontend/src/bundles/common/pages/landing/enums/enums.ts
+++ frontend/src/bundles/common/pages/landing/enums/enums.ts
@@ -1,4 +1,5 @@
 export { DoughnutChartCartVariant } from './doughnut-chart-card-variant.enum';
 export { StepInfoButtonVariant } from './step-info-button-variant.enum';
 export { StepInfoPanelVariant } from './step-info-panel-variant.enum';
+export { AppRoute } from '~/bundles/common/enums/app-route.enum';
 export { ButtonVariant } from '~/bundles/common/enums/button-variant.enum';

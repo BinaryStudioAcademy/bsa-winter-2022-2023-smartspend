2592b181e1b45f08b5dfaad87454a0dafe09ee8d
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-03-18 23:45:43 +0200

    ss-71: + lib FontAwesomeIcon

--- frontend/src/bundles/common/pages/categories-settings.tsx
+++ frontend/src/bundles/common/pages/categories-settings.tsx
@@ -1,5 +1,13 @@
+import { faEnvelope, faGasPump } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const CategoriesSettings: React.FC = () => {
-    return <div></div>;
+    return (
+        <div>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faGasPump} />
+        </div>
+    );
 };
 
 export { CategoriesSettings };

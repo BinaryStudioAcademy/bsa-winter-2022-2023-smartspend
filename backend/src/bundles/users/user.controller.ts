02749164d8d739de6d683686232d087d98e22619
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-03-22 19:53:45 +0200

    ss-68: * update swagger put user profile

--- backend/src/bundles/users/user.controller.ts
+++ backend/src/bundles/users/user.controller.ts
@@ -74,43 +74,62 @@ class UserController extends Controller {
     }
 
     /**
-     * @swagger
-     * /users:
-     * [tag]                              {* todo add correct tag swagger *}
-     *    put:
-     *      description: Updates a user
-     *      parameters:
-     *        - name: payload
-     *          in: body
-     *          required: true
-     *          schema:
-     *            type: object
-     *            properties:
-     *              email:
-     *                type: string
-     *                format: email
-     *              firstName:
-     *                type: string
-     *              lastName:
-     *                type: string
-     *              sex:
-     *                type: string
-     *                enum: [male, female]
-     *              dateOfBirth:
-     *                type: string
-     *                format: date
-     *              language:
-     *                type: string
-     *              currency:
-     *                type: string
-     *      responses:
-     *        200:
-     *          description: Successful operation
-     *          content:
-     *            application/json:
-     *              schema:
-     *                $ref: '#/components/schemas/User'
-     */
+ * @swagger
+ * /users:
+ *   put:
+ *     tags:
+ *       - Users
+ *     description: Updates a user profile
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 nullable: true
+ *               userProfile:
+ *                 type: object
+ *                 properties:
+ *                   firstName:
+ *                     type: string
+ *                     nullable: true
+ *                   lastName:
+ *                     type: string
+ *                     nullable: true
+ *                   sex:
+ *                     type: string
+ *                     enum: [male, female]
+ *                     nullable: true
+ *                   dateOfBirth:
+ *                     type: string
+ *                     format: date
+ *                     nullable: true
+ *                   language:
+ *                     type: string
+ *                     nullable: true
+ *                   currency:
+ *                     type: string
+ *                     nullable: true
+ *     responses:
+ *       '200':
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *   components:
+ *     securitySchemes:
+ *       bearerAuth:
+ *         type: http
+ *         scheme: bearer
+ */
+
     private async update(
         request: UpdateRequestDto,
     ): Promise<ApiHandlerResponse> {

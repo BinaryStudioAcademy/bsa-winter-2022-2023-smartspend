a9e3e4afff5071baae4c364d9917c4dea0078d2d
teslmik <teslmik@gmail.com>
2023-03-23 18:52:15 +0200

    ss-135: * validation signUp

--- shared/src/bundles/users/validation-schemas/user-sign-up.validation-schema.ts
+++ shared/src/bundles/users/validation-schemas/user-sign-up.validation-schema.ts
@@ -8,7 +8,7 @@ const userSignUp = joi.object<UserSignUpRequestDto, true>({
         .string()
         .pattern(
             new RegExp(
-                '^[a-zA-Z0-9._%+-]{1,15}@[a-zA-Z0-9.-]{4,15}\\.[a-zA-Z]{2,}$',
+                '^[a-zA-Z0-9._%+-]{1,15}@(?=.{3,15}$)[a-zA-Z0-9.-]{1,}\\.[a-zA-Z]{1,}$',
             ),
         )
         .email({

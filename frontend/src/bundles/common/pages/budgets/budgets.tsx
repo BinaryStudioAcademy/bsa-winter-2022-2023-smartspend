30f6e5f51ce559c33a9f5a5ce2071b2f061cea0d
teslmik <teslmik@gmail.com>
2023-03-24 18:05:51 +0200

    ss-126: * add props in Budget

--- frontend/src/bundles/common/pages/budgets/budgets.tsx
+++ frontend/src/bundles/common/pages/budgets/budgets.tsx
@@ -1,80 +1,53 @@
-import {
-    BudgetCard,
-    Button,
-    Header,
-} from '~/bundles/common/components/components';
-import { AppRoute, ButtonVariant } from '~/bundles/common/enums/enums';
+import { BudgetCard, Button } from '~/bundles/common/components/components';
+import { ButtonVariant } from '~/bundles/common/enums/enums';
 
 import { useCallback } from '../../hooks/hooks';
 import styles from './styles.module.scss';
 
-const tabsDashboard = [
-    { title: 'Dashboard', to: AppRoute.DASHBOARD },
-    { title: 'Budget', to: AppRoute.BUDGETS },
-];
-
-const tabsData = [
-    { title: 'Transaction', to: '/ui/' },
-    { title: 'Overview', to: '/ui/overview' },
-    { title: 'Budget', to: '/ui/budget' },
-    { title: 'Wallet Settings', to: '/ui/wallet-settings' },
-];
-
-const allTabsData = {
-    dashboard: tabsDashboard,
-    wallets: tabsData,
+type CardType = {
+    title: string;
+    total: number;
+    moneyLeft: number;
 };
 
-const Budgets: React.FC = () => {
+type Properties = {
+    budgetCards?: CardType[];
+};
+
+const Budgets: React.FC<Properties> = ({ budgetCards }) => {
     const handleClickCreate = useCallback((): void => {
         // handle create budget
     }, []);
+
     return (
-        <>
-            <Header dataTabs={allTabsData} />
-            <div className={styles.budgets}>
-                <div className={styles.container}>
-                    <div className={styles.wrapper}>
-                        <h1 className={styles.title}>Budgets</h1>
-                        <div className={styles.cards}>
+        <div className={styles.budgets}>
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <h1 className={styles.title}>Budgets</h1>
+                    <div className={styles.cards}>
+                        {budgetCards?.map((card, index) => (
                             <BudgetCard
-                                title={'One'}
-                                total={12_301.25}
-                                moneyLeft={824.56}
+                                key={index}
+                                title={card.title}
+                                total={card.total}
+                                moneyLeft={card.moneyLeft}
                             />
-                            <BudgetCard
-                                title={'Two'}
-                                total={1301}
-                                moneyLeft={135.45}
-                            />
-                            <BudgetCard
-                                title={'Three'}
-                                total={15_381}
-                                moneyLeft={1025.26}
-                            />
-                            <BudgetCard
-                                title={'Four'}
-                                total={75_471}
-                                moneyLeft={20_456}
-                            />
-                            <div
-                                className={styles.cardCreate}
-                                onClickCapture={handleClickCreate}
-                            >
-                                <div className={styles.cardWrapper}>
-                                    <Button variant={ButtonVariant.ROUND}>
-                                        +
-                                    </Button>
-                                    <p className={styles.createText}>
-                                        Create a New Budget
-                                    </p>
-                                </div>
+                        ))}
+                        <div
+                            className={styles.cardCreate}
+                            onClickCapture={handleClickCreate}
+                        >
+                            <div className={styles.cardWrapper}>
+                                <Button variant={ButtonVariant.ROUND}>+</Button>
+                                <p className={styles.createText}>
+                                    Create a New Budget
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 

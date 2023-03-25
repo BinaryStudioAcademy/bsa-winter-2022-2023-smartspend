a9974756526552e7d1913f40b64b7041918d93d0
teslmik <teslmik@gmail.com>
2023-03-25 22:20:41 +0200

    ss-152: * feedback-part responsive

--- frontend/src/bundles/landing/components/feedbacks-part/feedbacks-part.tsx
+++ frontend/src/bundles/landing/components/feedbacks-part/feedbacks-part.tsx
@@ -12,18 +12,20 @@ type Properties = {
 
 const FeedbacksPart: React.FC<Properties> = ({ title, feedbacks }) => {
     return (
-        <section className={styles.container}>
-            <h1 className={styles.title}>{title}</h1>
-            <div className={styles.carouselContainer}>
-                {feedbacks.map((feedback, index) => (
-                    <FeedbackCard
-                        name={feedback.name}
-                        avatar_src={feedback.src}
-                        feedback={feedback.feedback}
-                        gradient_number={index + 1}
-                        key={index}
-                    />
-                ))}
+        <section className={styles.body}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>{title}</h1>
+                <div className={styles.carouselContainer}>
+                    {feedbacks.map((feedback, index) => (
+                        <FeedbackCard
+                            name={feedback.name}
+                            avatar_src={feedback.src}
+                            feedback={feedback.feedback}
+                            gradient_number={index + 1}
+                            key={index}
+                        />
+                    ))}
+                </div>
             </div>
         </section>
     );

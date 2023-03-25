9e914f0dbf49450e44edbf322fb9f6aa6be89c3a
teslmik <teslmik@gmail.com>
2023-03-25 22:34:12 +0200

    ss-152: * subscription-part responsive

--- frontend/src/bundles/landing/components/subscription-part/subscription-part.tsx
+++ frontend/src/bundles/landing/components/subscription-part/subscription-part.tsx
@@ -18,22 +18,24 @@ const SubscriptionPart: React.FC<Properties> = ({ title }) => {
     });
 
     return (
-        <section className={styles.container}>
-            <h1 className={styles.title}>{title}</h1>
-            <form className={styles.form}>
-                <div className={styles.formContainer}>
-                    <Input
-                        type={InputType.TEXT}
-                        placeholder="Enter your email"
-                        name="email"
-                        control={control}
-                        errors={errors}
-                        labelClassName={styles.inputLabel}
-                        inputClassName={styles.input}
-                    />
-                    <Button>Subscribe</Button>
-                </div>
-            </form>
+        <section className={styles.body}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>{title}</h1>
+                <form className={styles.form}>
+                    <div className={styles.formContainer}>
+                        <Input
+                            type={InputType.TEXT}
+                            placeholder="Enter your email"
+                            name="email"
+                            control={control}
+                            errors={errors}
+                            labelClassName={styles.inputLabel}
+                            inputClassName={styles.input}
+                        />
+                        <Button>Subscribe</Button>
+                    </div>
+                </form>
+            </div>
         </section>
     );
 };

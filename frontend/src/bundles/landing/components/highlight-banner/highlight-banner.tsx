62ed0c6ee708ccf91b899b7c28c007266bbe2afa
teslmik <teslmik@gmail.com>
2023-03-25 12:35:03 +0200

    ss-152: * add container, fix responsive

--- frontend/src/bundles/landing/components/highlight-banner/highlight-banner.tsx
+++ frontend/src/bundles/landing/components/highlight-banner/highlight-banner.tsx
@@ -12,38 +12,39 @@ type Properties = {
 
 const HighlightBanner: React.FC<Properties> = ({ title, details }) => {
     return (
-        <section className={styles.container}>
-            <span className={styles.pinkCircle}></span>
-            <span className={styles.violetCircle}></span>
-            <span className={styles.blueCircle}></span>
-
-            <div className={styles.detailsContainer}>
-                <h1 className={styles.title}>{title}</h1>
-                <p className={styles.details}>{details}</p>
-                <div className={styles.buttonsContainer}>
-                    <StoreButton
-                        url={'https://www.apple.com/app-store/'}
-                        title={'Download on the'}
-                        body={'App Store'}
-                        iconPath={AppStoreIcon}
-                        storeAlt={'app store'}
-                    />
-                    <StoreButton
-                        url={'https://play.google.com/'}
-                        title={'Download on the'}
-                        body={'Google Play'}
-                        iconPath={GooglePlayIcon}
-                        storeAlt={'google play'}
+        <section className={styles.body}>
+            <div className={styles.container}>
+                <span className={styles.pinkCircle}></span>
+                <span className={styles.violetCircle}></span>
+                <span className={styles.blueCircle}></span>
+                <div className={styles.detailsContainer}>
+                    <h1 className={styles.title}>{title}</h1>
+                    <p className={styles.details}>{details}</p>
+                    <div className={styles.buttonsContainer}>
+                        <StoreButton
+                            url={'https://www.apple.com/app-store/'}
+                            title={'Download on the'}
+                            body={'App Store'}
+                            iconPath={AppStoreIcon}
+                            storeAlt={'app store'}
+                        />
+                        <StoreButton
+                            url={'https://play.google.com/'}
+                            title={'Download on the'}
+                            body={'Google Play'}
+                            iconPath={GooglePlayIcon}
+                            storeAlt={'google play'}
+                        />
+                    </div>
+                </div>
+                <div className={styles.laptopImgWrapper}>
+                    <img
+                        src={LaptopPreview}
+                        alt="laptop"
+                        className={styles.laptopImg}
                     />
                 </div>
             </div>
-            <img
-                src={LaptopPreview}
-                alt="laptop"
-                width={'608'}
-                height={'327'}
-                className={styles.laptopImg}
-            />
         </section>
     );
 };

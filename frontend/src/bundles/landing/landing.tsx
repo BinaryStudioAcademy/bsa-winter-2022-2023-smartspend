63536ae30ca4c4cea6ee3a74214db994baeb791e
teslmik <teslmik@gmail.com>
2023-03-26 17:02:29 +0300

    ss-126: * fix landing mock data

--- frontend/src/bundles/landing/landing.tsx
+++ frontend/src/bundles/landing/landing.tsx
@@ -4,33 +4,14 @@ import {
     FeedbacksPart,
     FirstStepPart,
     Footer,
-    Header,
     HighlightBanner,
     QuestionPart,
     SecondStepPart,
     SubscriptionPart,
     ThirdStepPart,
 } from './components/components';
-import { AppRoute } from './enums/enums';
 
 const Landing: React.FC = () => {
-    const tabsData = [
-        { title: 'Transaction', to: '/ui/' },
-        { title: 'Overview', to: '/ui/overview' },
-        { title: 'Budget', to: '/ui/budget' },
-        { title: 'Wallet Settings', to: '/ui/wallet-settings' },
-    ];
-
-    const tabsDashboard = [
-        { title: 'Dashboard', to: AppRoute.DASHBOARD },
-        { title: 'Budget', to: AppRoute.BUDGETS },
-    ];
-
-    const allTabsData = {
-        dashboard: tabsDashboard,
-        wallets: tabsData,
-    };
-
     const feedbacksArray = [
         {
             name: 'Roy',

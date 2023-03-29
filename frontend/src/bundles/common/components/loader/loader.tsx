73dc470c239b0d4a9d30478fe1c796a8726a7984
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-03-29 19:28:12 +0200

    ss-171: + loader container

--- frontend/src/bundles/common/components/loader/loader.tsx
+++ frontend/src/bundles/common/components/loader/loader.tsx
@@ -3,7 +3,11 @@ import Logo from '~/assets/img/logo.svg';
 import styles from './styles.module.scss';
 
 const Loader: React.FC = () => {
-    return <img className={styles.spinner} src={Logo} alt="logo" />;
+    return (
+        <div className={styles.container}>
+            <img className={styles.spinner} src={Logo} alt="logo" />
+        </div>
+    );
 };
 
 export { Loader };

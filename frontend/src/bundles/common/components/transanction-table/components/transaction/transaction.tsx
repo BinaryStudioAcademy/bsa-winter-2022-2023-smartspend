1ba12e9fbf37334040c3e87b14c36f31aa9a4468
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-03-29 16:15:18 +0300

    ss-151: * add background if checked

--- frontend/src/bundles/common/components/transanction-table/components/transaction/transaction.tsx
+++ frontend/src/bundles/common/components/transanction-table/components/transaction/transaction.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import {
     type Control,
     type FieldErrors,
@@ -10,7 +10,6 @@ import OrangeMurseIcon from '~/assets/img/orange-murse-icon.svg';
 import { Input } from '~/bundles/common/components/components';
 import { getDaysLeft } from '~/bundles/common/components/transanction-table/helpers/index';
 import { InputType } from '~/bundles/common/enums/enums';
-import { useCallback } from '~/bundles/common/hooks/hooks';
 
 import { type ITransaction } from '../../types';
 import styles from '../styles.module.scss';
@@ -27,45 +26,52 @@ const Transaction: React.FC<TransactionProperties> = ({
     errors,
     isFutureTransaction = false,
 }) => {
-    const handleOnChange = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>): void => {
-            if (event.target.checked) {
-                // Remove log and do there what you need
-                // eslint-disable-next-line no-console
-                console.log(`Checkbox ${transaction.id} is checked`);
-            }
-        },
-        [transaction.id],
-    );
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleChange = (event: React.FormEvent<HTMLFormElement>): void => {
+        const target = event.target as HTMLInputElement;
+        if (target.checked) {
+            // eslint-disable-next-line no-console
+            console.log(`Checkbox ${transaction.id} is checked`);
+            setIsChecked(true);
+        } else {
+            setIsChecked(false);
+        }
+    };
 
     return (
-        <div key={transaction.id} className={styles.transaction}>
+        <div
+            key={transaction.id}
+            className={classNames(
+                styles.transaction,
+                isChecked ? styles.checked : styles.unchecked,
+            )}
+        >
             <div className={classNames(styles.columns, styles.leftColumn)}>
-                <Input
-                    type={InputType.CHECKBOX}
-                    name={`checkbox-${transaction.id}`}
-                    control={control}
-                    errors={errors}
-                    inputClassName={styles.checkbox}
-                    onChange={handleOnChange}
-                />
+                {/* eslint-disable-next-line react/jsx-no-bind  */}
+                <form className={styles.form} onChange={handleChange}>
+                    <Input
+                        type={InputType.CHECKBOX}
+                        name={`checkbox-${transaction.id}`}
+                        control={control}
+                        errors={errors}
+                        inputClassName={styles.checkbox}
+                    />
+                </form>
                 <div>
                     {/* There must be icon from DB */}
                     <img src={OrangeMurseIcon} alt={'murse'} />
                 </div>
-
                 <div>{transaction.category}</div>
             </div>
-            <div>
-                {isFutureTransaction && (
-                    <>
-                        <div>{transaction.date}</div>
-                        <div className={styles.totals}>
-                            in {getDaysLeft([transaction])} days
-                        </div>
-                    </>
-                )}
-            </div>
+            {isFutureTransaction && (
+                <div className={styles.inDays}>
+                    <span>{transaction.date}</span>
+                    <span className={styles.totals}>
+                        in {getDaysLeft([transaction])} days
+                    </span>
+                </div>
+            )}
             <div
                 className={classNames(
                     styles.columns,

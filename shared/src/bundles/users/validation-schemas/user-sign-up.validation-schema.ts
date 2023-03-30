b780e7e16bcd225e83eb57b0f1bfd7ee83da2154
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-03-30 23:08:56 +0300

    ss-219: + ability to use new characters for email field

--- shared/src/bundles/users/validation-schemas/user-sign-up.validation-schema.ts
+++ shared/src/bundles/users/validation-schemas/user-sign-up.validation-schema.ts
@@ -3,9 +3,8 @@ import joi from 'joi';
 import { UserValidationMessage } from '../enums/enums.js';
 import { type UserSignUpRequestDto } from '../types/types.js';
 
-const emailRegExp = new RegExp(
-    '^[a-zA-Z0-9._%+-]{1,15}@(?=.{3,15}$)[a-zA-Z0-9.-]{1,}\\.[a-zA-Z]{1,}$',
-);
+const emailRegExp =
+    /^[\w!#$%&'*+./=?^`{|}~-]+@[\dA-Za-z]+(?:[\w-]+\.)+[A-Za-z]{2,}$/;
 
 const userSignUp = joi.object<UserSignUpRequestDto, true>({
     email: joi

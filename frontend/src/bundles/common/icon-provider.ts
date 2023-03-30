fef6bac6a5abbe467f767d29db0b2931c70ee4f3
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-03-30 23:02:46 +0300

    ss-167: * add icon right chevron

--- frontend/src/bundles/common/icon-provider.ts
+++ frontend/src/bundles/common/icon-provider.ts
@@ -1,6 +1,6 @@
 import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
-import { faChevronDown, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronRight,faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const iconProvider = { faChevronDown, faEye, faEyeSlash, faPlus };
+const iconProvider = { faChevronDown, faEye, faEyeSlash, faPlus, faChevronRight };
 
 export { iconProvider };

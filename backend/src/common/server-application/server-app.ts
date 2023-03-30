0fa20e366205a5dfba5919bb694107ca9bc89801
Vitaliy Kalenichenko <vitaliy.kalenichenko@binary-studio.com>
2023-03-31 01:01:44 +0300

    ss-00: * Uncommented initPlugins in server-app.ts

--- backend/src/common/server-application/server-app.ts
+++ backend/src/common/server-application/server-app.ts
@@ -174,14 +174,13 @@ class ServerApp implements IServerApp {
 
     public async init(): Promise<void> {
         this.logger.info('Application initialization…');
-
         await this.app.register(cors, {
             origin: true,
         });
 
         await this.initMiddlewares();
 
-        // await this.initPlugins();
+        await this.initPlugins();
 
         this.initValidationCompiler();
 

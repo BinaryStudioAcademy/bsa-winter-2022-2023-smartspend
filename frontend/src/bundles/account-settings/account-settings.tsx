ddc05b1c6e71e691345c892382753d1372635df9
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-04-02 11:56:02 +0200

    ss-251: + user settings page

---
+++ frontend/src/bundles/account-settings/account-settings.tsx
@@ -0,0 +1,23 @@
+import { AppRoute } from '../common/enums/app-route.enum';
+import { Tabs, UserProfile } from './components/components';
+import styles from './styles.module.scss';
+
+const AccountSettings: React.FC = () => {
+    const dataTabs = [
+        { title: 'Account', to: AppRoute.USER },
+        { title: 'All Categories', to: AppRoute.DASHBOARD },
+        { title: 'Connected bank accounts', to: AppRoute.DASHBOARD },
+        { title: 'Support', to: AppRoute.DASHBOARD },
+        { title: 'Terms and Policies', to: AppRoute.DASHBOARD },
+    ];
+    return (
+        <main className={styles.container}>
+            <Tabs tabsData={dataTabs} />
+            <div className={styles.userProfileContainer}>
+                <UserProfile />
+            </div>
+        </main>
+    );
+};
+
+export { AccountSettings };

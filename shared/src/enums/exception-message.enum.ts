33680679510c24b75cf76275aff4aaaf2bd42cc1
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-04-03 18:12:28 +0300

    ss-227: * undefined found route path

--- shared/src/enums/exception-message.enum.ts
+++ shared/src/enums/exception-message.enum.ts
@@ -2,6 +2,7 @@ enum ExceptionMessage {
     INVALID_CREDENTIALS = 'You entered an incorrect e-mail address or password',
     EMAIL_ALREADY_EXISTS = 'User with this email already exists',
     INVALID_TOKEN = 'Invalid token',
+    JWT_MUST_BE_PROVIDED = 'jwt token must be provided',
 }
 
 export { ExceptionMessage };

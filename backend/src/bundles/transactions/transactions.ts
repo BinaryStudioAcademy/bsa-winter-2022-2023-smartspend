5fd9c8bdce24d64f0dc50b006049acd190b788f0
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-04-03 16:35:27 +0300

    ss-128: + new imports

--- backend/src/bundles/transactions/transactions.ts
+++ backend/src/bundles/transactions/transactions.ts
@@ -1,1 +1,4 @@
+export { TransactionEntity } from './transaction.entity';
 export { TransactionModel } from './transaction.model';
+export { TransactionRepository } from './transaction.repository';
+export { TransactionService } from './transaction.service';

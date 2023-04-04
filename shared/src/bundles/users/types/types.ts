7f592cdede6421a16d210a02b48622e64392a044
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-04-04 22:23:24 +0300

    ss-292: * run prettier

--- shared/src/bundles/users/types/types.ts
+++ shared/src/bundles/users/types/types.ts
@@ -1,7 +1,7 @@
 export { type UpdateRequestDto } from './update-request-dto';
 export { type UserGetAllItemResponseDto } from './user-get-all-item-response-dto.type.js';
 export { type UserGetAllResponseDto } from './user-get-all-response-dto.type.js';
-export { type UserProfileResponseDto }from './user-get-user-profile-response-dto.type.js';
+export { type UserProfileResponseDto } from './user-get-user-profile-response-dto.type.js';
 export { type UserLoadRequestDto } from './user-load-request-dto.type.js';
 export { type UserLoadResponseDto } from './user-load-response-dto.type.js';
 export { type UserSignInRequestDto } from './user-sign-in-request-dto.type.js';
--- shared/src/bundles/users/types/user-get-user-profile-response-dto.type.ts
+++ shared/src/bundles/users/types/user-get-user-profile-response-dto.type.ts
@@ -1,15 +1,15 @@
 type UserProfileResponseDto = {
-  email: string;
-  id: string | null;
-  createdAt: string | null;
-  updatedAt: string | null;
-  userId: string | null;
-  firstName: string | null;
-  lastName: string | null;
-  sex: string | null;
-  dateOfBirth: string | null;
-  language: string | null;
-  currency: string | null;
+    email: string;
+    id: string | null;
+    createdAt: string | null;
+    updatedAt: string | null;
+    userId: string | null;
+    firstName: string | null;
+    lastName: string | null;
+    sex: string | null;
+    dateOfBirth: string | null;
+    language: string | null;
+    currency: string | null;
 };
 
 export { type UserProfileResponseDto };

2fa21fd12228ac7952aeac9d7f0c157b28ca05fc
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-04-04 22:08:50 +0300

    ss-292: * add route users/user to get user profile info

--- backend/src/bundles/users/user.controller.ts
+++ backend/src/bundles/users/user.controller.ts
@@ -1,10 +1,8 @@
 import { type UpdateRequestDto } from 'shared/build';
 
 import { type UserService } from '~/bundles/users/user.service.js';
-import {
-    type ApiHandlerResponse,
-    Controller,
-} from '~/common/controller/controller.js';
+import { type ApiHandlerResponse } from '~/common/controller/controller.js';
+import { Controller } from '~/common/controller/controller.js';
 import { ApiPath } from '~/common/enums/enums.js';
 import { getUserIdFromToken } from '~/common/helpers/get-id-from-token.helper.js';
 import { HttpCode } from '~/common/http/http.js';
@@ -43,6 +41,12 @@ class UserController extends Controller {
         });
 
         this.addRoute({
+            path: UsersApiPath.GET_ONE_USER,
+            method: 'GET',
+            handler: (options) => this.find({ token: options.token as string }),
+        });
+
+        this.addRoute({
             path: UsersApiPath.ROOT,
             method: 'PUT',
             handler: (options) => {
@@ -71,6 +75,74 @@ class UserController extends Controller {
         return {
             status: HttpCode.OK,
             payload: await this.userService.findAll(),
+        };
+    }
+
+    /**
+     * @swagger
+     * /users/user:
+     *   get:
+     *     summary: Get user information
+     *     description: Get information about the user associated with the specified access token.
+     *     tags: [Users]
+     *     security:
+     *       - BearerAuth: []
+     *     parameters:
+     *       - in: header
+     *         name: Authorization
+     *         description: Access token for authorization
+     *         required: true
+     *         schema:
+     *           type: string
+     *     responses:
+     *       200:
+     *         description: The user information was successfully retrieved.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/UserProfile'
+     * components:
+     *   schemas:
+     *     UserProfile:
+     *       type: object
+     *       properties:
+     *         email:
+     *           type: string
+     *           format: email
+     *         id:
+     *           type: string
+     *           format: uuid
+     *         createdAt:
+     *           type: string
+     *           format: date-time
+     *         updatedAt:
+     *           type: string
+     *           format: date-time
+     *         userId:
+     *           type: string
+     *           format: uuid
+     *         firstName:
+     *           type: string
+     *         lastName:
+     *           type: string
+     *         sex:
+     *           type: string
+     *         dateOfBirth:
+     *           type: string
+     *           format: date-time
+     *         language:
+     *           type: string
+     *         currency:
+     *           type: string
+     */
+
+    private async find(options: {
+        token: string;
+    }): Promise<ApiHandlerResponse> {
+        const userId = getUserIdFromToken(options.token);
+        return {
+            status: HttpCode.OK,
+            payload: await this.userService.findUserInfo(userId),
         };
     }
 

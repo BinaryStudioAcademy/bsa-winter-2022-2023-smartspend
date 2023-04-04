cf4a83616dd9500cd2c74647049e695e87b38519
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-04-04 14:46:01 +0300

    ss-167: * add categories icons to dropdown

--- frontend/src/bundles/common/pages/budgets/budget-details/components/multi-dropdown/multi-dropdown.tsx
+++ frontend/src/bundles/common/pages/budgets/budget-details/components/multi-dropdown/multi-dropdown.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useCallback, useState } from 'react';
 import { type MultiValue, type SingleValue } from 'react-select';
 
@@ -5,6 +6,63 @@ import { MultiDropdown } from '~/bundles/common/components/components';
 import { type DataType } from '~/bundles/common/types/dropdown.type';
 
 import styles from './styles.module.scss';
+
+const dataMenu = [
+    {
+        id: '6b6510e3-7bd9-4952-9db4-e97a03dce2f6',
+        name: 'Food & Drink',
+        icon: 'burger',
+        color: 'red',
+        type: 'expense',
+    },
+    {
+        id: '0a478b81-0f6e-4e04-810d-13b18f210954',
+        name: 'Salary',
+        icon: 'money-bill',
+        color: 'green',
+        type: 'income',
+    },
+    {
+        id: 'd2303d78-9446-471d-80c2-6966b6ceed3b',
+        name: 'Car',
+        icon: 'car',
+        color: 'blue',
+        type: 'expense',
+    },
+    {
+        id: '5a832c55-954e-4def-8196-5854849b1977',
+        name: 'Travel',
+        icon: 'plane',
+        color: 'pink',
+        type: 'expense',
+    },
+    {
+        id: 'e4aec229-ff61-49b8-9da8-d4209427ac76',
+        name: 'Gifts',
+        icon: 'gifts',
+        color: 'green',
+        type: 'income',
+    },
+    {
+        id: '9ec27eab-1737-44eb-8e7b-56ab214dffc4',
+        name: 'Gifts',
+        icon: 'gifts',
+        color: 'red',
+        type: 'expense',
+    },
+    {
+        id: '2debaea0-d8d9-4c3d-9fb4-e1b375b62318',
+        name: 'Bills & Fees',
+        icon: 'money-bills',
+        color: 'red',
+        type: 'expense',
+    },
+] as DataType[];
+
+const newDataMenu = dataMenu.map((item) => ({
+    ...item,
+    value: item.id ?? '',
+}));
 
 const RenderMultiDropdown = ({
     field: { onChange },
@@ -45,41 +103,32 @@ const RenderMultiDropdown = ({
         [handleMultiDropdownChange, onChange],
     );
 
-    // todo FontAwesomeIcon to dropdown
+    const formatOptionLabel = useCallback(
+        (data: DataType): JSX.Element => (
+            <div className={styles.item}>
+                <input
+                    type="checkbox"
+                    checked={(selectedMulti as MultiValue<DataType>).some(
+                        (option) => option.value === data.value,
+                    )}
+                    readOnly
+                    className={styles.checkbox}
+                />
+
+                {data.icon && (
+                    <FontAwesomeIcon icon={data.icon} color={data.color} />
+                )}
+                {data.icon && <span className={styles.name}>{data.name}</span>}
+            </div>
+        ),
+        [selectedMulti],
+    );
 
     return (
         <div className={styles.multiDropdown}>
             <MultiDropdown
-                data={[
-                    {
-                        value: '6b6510e3-7bd9-4952-9db4-e97a03dce2f6',
-                        name: 'Food & Drink',
-                    },
-                    {
-                        value: '0a478b81-0f6e-4e04-810d-13b18f210954',
-                        name: 'Salary',
-                    },
-                    {
-                        value: 'd2303d78-9446-471d-80c2-6966b6ceed3b',
-                        name: 'Car',
-                    },
-                    {
-                        value: '5a832c55-954e-4def-8196-5854849b1977',
-                        name: 'Travel',
-                    },
-                    {
-                        value: 'e4aec229-ff61-49b8-9da8-d4209427ac76',
-                        name: 'Gifts',
-                    },
-                    {
-                        value: '9ec27eab-1737-44eb-8e7b-56ab214dffc4',
-                        name: 'Gifts',
-                    },
-                    {
-                        value: '2debaea0-d8d9-4c3d-9fb4-e1b375b62318',
-                        name: 'Bills & Fees',
-                    },
-                ]}
+                formatOptionLabel={formatOptionLabel}
+                data={newDataMenu}
                 label={'Budgeted for'}
                 selectedOption={selectedMulti}
                 handleChange={handleMultiDropdownChangeWrapper}

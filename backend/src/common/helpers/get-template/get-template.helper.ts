fa46b57150fc43acee5f216bad37172627fdb6b8
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-04-05 09:51:49 +0300

    ss-262: * folder name

--- backend/src/common/helpers/get-template/get-template.helper.ts
+++ backend/src/common/helpers/get-template/get-template.helper.ts
@@ -13,7 +13,7 @@ const getTemplate = ({ name, context }: Properties): string => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
     const emailTemplateSource = fs.readFileSync(
-        path.join(__dirname, `../../../views/${name}.hbs`),
+        path.join(__dirname, `../../../templates/${name}.hbs`),
         'utf8',
     );
     const template = handlebars.compile(emailTemplateSource);

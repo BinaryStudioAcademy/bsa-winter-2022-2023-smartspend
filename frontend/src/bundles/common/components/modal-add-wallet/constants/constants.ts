46b9991aab1a5461d5be419f6967b97d28b57d28
teslmik <teslmik@gmail.com>
2023-04-05 12:39:15 +0300

    ss-213: * constants

--- frontend/src/bundles/common/components/modal-add-wallet/constants/constants.ts
+++ frontend/src/bundles/common/components/modal-add-wallet/constants/constants.ts
@@ -1,21 +1,14 @@
 const currencies = [
-    { value: '1f7200b3-2ddb-4aa7-a621-17522e007322', name: 'US Dollar' },
-    { value: '6ce617b2-9f45-4fc9-a641-b01fc6f8d3c0', name: 'Euro' },
-    { value: 'ec13ca2c-1d91-41e3-8a1a-b29c80df28c8', name: 'British Pound' },
-    {
-        value: 'e2b321a3-807d-445b-bd22-34856b6edadd',
-        name: 'Australian Dollar',
-    },
-    { value: 'a51a9b71-bc98-4453-9f57-908e6c0455fd', name: 'Canadian Dollar' },
-    { value: 'f3ac5b5a-c83c-4df1-b9dd-540c34239e60', name: 'Japanese Yen' },
-    { value: '1d2c60a2-26bd-4adb-a16f-dcc7ea0cb2d5', name: 'Swiss Franc' },
-    { value: 'e783b214-27be-4831-b274-e34d68b8af7d', name: 'Chinese Yuan' },
-    { value: '3e47d198-96b6-4c4d-985d-2b35991df53d', name: 'Hong Kong Dollar' },
-    { value: '00df4390-26e4-460c-8010-f5897719e157', name: 'Singapore Dollar' },
-    {
-        value: '4eb6b85d-829d-4e10-9db3-4f03b92cb18b',
-        name: 'Ukrainian Hryvnia',
-    },
+    { value: 'USD', name: 'US Dollar' },
+    { value: 'EUR', name: 'Euro' },
+    { value: 'GBP', name: 'British Pound' },
+    { value: 'AUD', name: 'Australian Dollar' },
+    { value: 'CAD', name: 'Canadian Dollar' },
+    { value: 'JPY', name: 'Japanese Yen' },
+    { value: 'CHF', name: 'Swiss Franc' },
+    { value: 'CNY', name: 'Chinese Yuan' },
+    { value: 'HKD', name: 'Hong Kong Dollar' },
+    { value: 'SGD', name: 'Singapore Dollar' },
 ];
 
 export { currencies };

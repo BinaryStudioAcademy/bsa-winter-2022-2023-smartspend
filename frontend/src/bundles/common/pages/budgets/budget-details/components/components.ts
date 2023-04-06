3963e5b1d18ad8104ac9a2984c327228de436364
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-04-06 11:17:20 +0300

    ss-167: * rename modal to budget-modal

--- frontend/src/bundles/common/pages/budgets/budget-details/components/components.ts
+++ frontend/src/bundles/common/pages/budgets/budget-details/components/components.ts
@@ -1,6 +1,6 @@
+export { EditBudgetModal } from './budget-modal/budget-modal';
 export { BudgetProgressBar } from './budget-progress-bar/budget-progress-bar';
 export { RenderCurrency } from './currency/currency';
-export { EditBudgetModal } from './edit-budget-modal/edit-budget-modal';
 export { InfoCard } from './info-card/info-card';
 export { RenderMultiDropdown } from './multi-dropdown/multi-dropdown';
 export { RenderDate } from './one-day-calendar/one-day-calendar';

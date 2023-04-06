5c2bb4d900329df96146310ba4500d1e457578f7
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-04-06 09:27:46 +0300

    ss-245: * prettified

--- frontend/src/bundles/common/helpers/get-app-title/get-app-title.helper.ts
+++ frontend/src/bundles/common/helpers/get-app-title/get-app-title.helper.ts
@@ -1,7 +1,7 @@
 import { BASE_APP_TITLE } from '../../constants/base-app-title.constant';
 
 const getAppTitle = (title?: string): string => {
-    if(!title) {
+    if (!title) {
         return BASE_APP_TITLE;
     }
     const appTitlesArray = [title, BASE_APP_TITLE];

d7869b4150cebc84934e15891bfdccb9838877b1
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-04-06 16:40:03 +0300

    ss-306: + main files

---
+++ frontend/src/bundles/common/pages/wallet-settings/wallet-settings.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const WalletSettings: React.FC = () => {
+    return <div className={styles.wrapper}>wallet-settings</div>;
+};
+
+export { WalletSettings };

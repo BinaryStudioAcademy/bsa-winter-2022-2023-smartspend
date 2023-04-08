80ee3ba567d70afde4ebf0f91a78d184c4b329e1
TarasMavrovich <taras.mavrovich@gmail.com>
2023-04-08 21:36:35 +0300

    ss-385: * fix styles update account, * fix/delete-avatar

--- frontend/src/bundles/account-settings/components/settings/user-profile/avatar-container.tsx
+++ frontend/src/bundles/account-settings/components/settings/user-profile/avatar-container.tsx
@@ -29,7 +29,11 @@ const AvatarContainer: React.FC = () => {
                 />
                 <div className={styles.btnContainer}>
                     <UploadAvatarButton handleFileChange={handleFileChange} />
-                    <DeleteAvatarButton deleteFile={deleteFIle} />
+                    {selectedFile ? (
+                        <DeleteAvatarButton deleteFile={deleteFIle} />
+                    ) : (
+                        ''
+                    )}
                 </div>
             </div>
         </div>

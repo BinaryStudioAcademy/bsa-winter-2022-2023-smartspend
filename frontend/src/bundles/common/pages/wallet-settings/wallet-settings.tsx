a55fdb41b98bbb8f4553917ff6a51040112168ae
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-04-08 16:06:01 +0300

    ss-306: + layout and styles

--- frontend/src/bundles/common/pages/wallet-settings/wallet-settings.tsx
+++ frontend/src/bundles/common/pages/wallet-settings/wallet-settings.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import { UserSettingsTabs as SettingsTabs } from '../../components/components';
+import {
+    ButtonsContainer,
+    InputsContainer,
+    Title,
+} from './components/components';
 import styles from './styles.module.scss';
 
 const WalletSettings: React.FC = () => {
-    return <div className={styles.wrapper}>wallet-settings</div>;
+    const tabsData = [
+        {
+            title: 'Main Settings',
+            to: '',
+            icon: 'SETTINGS',
+        },
+    ];
+    return (
+        <div className={styles.body}>
+            <div className={styles.container}>
+                <div className={styles.tabs_container}>
+                    <SettingsTabs tabsData={tabsData} />
+                </div>
+                <main className={styles.main_board}>
+                    <Title>General Information</Title>
+                    <InputsContainer />
+                    <ButtonsContainer />
+                </main>
+            </div>
+        </div>
+    );
 };
 
 export { WalletSettings };

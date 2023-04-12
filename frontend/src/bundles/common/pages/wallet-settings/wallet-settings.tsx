d14ef7915f40a2065c010929bc4317f6b0e23d38
Andrew <gandrejko2002@gmail.com>
2023-04-12 21:34:02 +0300

    ss-513: * refactor wallet settings to design

--- frontend/src/bundles/common/pages/wallet-settings/wallet-settings.tsx
+++ frontend/src/bundles/common/pages/wallet-settings/wallet-settings.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { UserSettingsTabs as SettingsTabs } from '../../components/components';
+import { UserSettingsTabs } from '../../components/components';
 import { FormContainer, Title } from './components/components';
 import styles from './styles.module.scss';
 
 const WalletSettings: React.FC = () => {
-    const tabsData = [
+    const dataTabs = [
         {
             title: 'Main Settings',
             to: '',
@@ -15,10 +15,10 @@ const WalletSettings: React.FC = () => {
     return (
         <div className={styles.body}>
             <div className={styles.container}>
-                <div className={styles.tabs_container}>
-                    <SettingsTabs tabsData={tabsData} />
+                <div className={styles.tabsContainer}>
+                    <UserSettingsTabs tabsData={dataTabs} />
                 </div>
-                <main className={styles.main_board}>
+                <main className={styles.mainBoard}>
                     <Title>General Information</Title>
                     <FormContainer />
                 </main>

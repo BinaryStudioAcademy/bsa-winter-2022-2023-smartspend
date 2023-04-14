a828999682144695fa85cfbf74e22e9eb8beae15
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2023-04-14 10:04:13 +0300

    ss-637: * fix id

--- frontend/src/bundles/common/pages/dashboard/helpers/helpers.tsx
+++ frontend/src/bundles/common/pages/dashboard/helpers/helpers.tsx
@@ -266,8 +266,8 @@ const processTransactions = (
     const negativeResult: ProcessedTransaction[] = [];
 
     for (const current of transactions) {
-        const categoryId = current.category?.id;
-        let gradient = gradientMap[current.category.id];
+        const categoryId = current.category.id;
+        let gradient = gradientMap[categoryId];
 
         if (!gradient) {
             gradient = gradientDoughnut.find(

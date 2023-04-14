d20a092d3ec51a53722d42c34c951e5bbcb062a6
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-04-14 08:42:09 +0300

    ss-606: * fix amount render Infinity

--- frontend/src/bundles/common/components/range-slider/range-slider.tsx
+++ frontend/src/bundles/common/components/range-slider/range-slider.tsx
@@ -54,8 +54,8 @@ const RangeSlider: React.FC<Properties> = ({
                 }
             />
             <div className={styles.rangeValues}>
-                <span>{currentRange.min}</span>
-                <span>{currentRange.max}</span>
+                <span>{Number.isFinite(currentRange.min) ? currentRange.min : 0}</span>
+                <span>{Number.isFinite(currentRange.max) ? currentRange.max : 0}</span>
             </div>
         </div>
     );
